ac35986cf3948e5610802214cb4cae6603c3f9ba
aokalugin <aokalugin@edu.hse.ru>
2022-04-22 01:50:43 +0300

    fixes

--- app/main.py
+++ app/main.py
@@ -36,4 +36,4 @@ async def destroy_ovpn():
 
 app.include_router(router)
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
